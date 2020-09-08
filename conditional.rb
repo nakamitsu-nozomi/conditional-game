59f807f3afe5f0f97e7b3bd1b0dbfc337fbba86c
nozomi <kkk@docomo.ne.jp>
2020-09-08 10:36:02 +0900

    登録

---
+++ conditional.rb
@@ -0,0 +1,16 @@
+# 勇者のHP
+brave_hp = 30
+# 勇者の攻撃力
+brave_attack = 10
+# 通車の防御力
+brave_defense = 5
+# 敵のHP
+enemy_hp = 30
+# 敵の攻撃力
+enemy_attack = 5
+# 敵の防御力
+enemy_defense = 10
+
+# 敵に与えるダメージ計算
+enemy_damege = brave_attack - enemy_defense
+#敵のHPにダメージを与える
