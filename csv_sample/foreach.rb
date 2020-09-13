f09bcc6defeece6e245c4e9be9db65ebd393f541
nozomi <kkk@docomo.ne.jp>
2020-09-13 14:23:09 +0900

    #csv 完了

--- csv_sample/foreach.rb
+++ csv_sample/foreach.rb
@@ -2,9 +2,26 @@ require "csv"
 require "pry"
 list=[]
 CSV.foreach("okinawa_area.csv",headers: true) do |row|
-  binding.pry
   list << { id: row["id"].to_i ,country: row["郡"],city: row["市区町村"],area: row["面積"].to_f}
 end 
-p list
+
+
+ 
+
+#id が 6 のデータ
+  p list.find{|data|data[:id]==6 }
+#南大東村 のデータ
+  p list.find{|data|data[:city]=="南大東村"}
+#面積 が 100 以上の全データ
+  p list.find_all{|data| data[:area]>=100}
+#中頭郡 の全データ
+  nakagami_list= list.find_all{|data| data[:country]=="中頭郡"}
+  p nakagami_list
+
+#中頭郡 の全市町村の配列
+ p nakagami_list.map { |data| data[:city] }
+#中頭郡 の総面積
+ puts nakagami_list.sum { |data| data[:area] }
+
 
  
--- csv_sample/read.rb
+++ csv_sample/read.rb
@@ -1,9 +1,8 @@
 require "csv"
 
-array=CSV.read("okinawa_area.csv")
-p array
+list=CSV.read("okinawa_area.csv",headers: true).map do |row|
+ {id: row["id"].to_i,country: row["郡"],city: row["市区町村"],area: row["面積"].to_f}
+end
+p list
 
-puts "1行目に対応する配列"
- p array[0]
- puts "2行目に対応する配列"
- p array[1]
+
