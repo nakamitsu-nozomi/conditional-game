efbf683b40fe4a7e7b059a647955cf45684d2b5c
nozomi <kkk@docomo.ne.jp>
2020-09-13 12:52:21 +0900

    ＃csv わからん

--- csv_sample/foreach.rb
+++ csv_sample/foreach.rb
@@ -3,18 +3,6 @@ require "pry"
 list=[]
 CSV.foreach("okinawa_area.csv",headers: true) do |row|
   binding.pry
-  row
-
-row[:id]
-row["id"]
-row["id"].to_i
-
-row["郡"]
-row["市区町村"]
-
-row["面積"]
-row["面積"].to_f
-
   list << { id: row["id"].to_i ,country: row["郡"],city: row["市区町村"],area: row["面積"].to_f}
 end 
 p list
