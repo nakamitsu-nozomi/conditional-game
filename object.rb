f3509f5123bc04184887db424aa25b0eaceea7e3
nozomi <kkk@docomo.ne.jp>
2020-09-13 11:29:11 +0900

    #object idわからん

--- object.rb
+++ object.rb
@@ -1,7 +1,7 @@
 class Task
   attr_accessor :id, :name, :detail
-  
   def initialize(params)
+    @id=
     @name = params[:name]
     @detail = params[:detail]
   end
@@ -22,10 +22,15 @@ class To_do
     puts "-------------"
     puts "<タスク一覧＞"
     @tasks.each.with_index(1) do |task,i|
-      puts "ID.#{i},タスク名:#{task.name},詳細:#{task.detail}"
+      puts "ID#{i},タスク名:#{task.name},詳細:#{task.detail}"
     end  
     puts "-------------" 
   end
+  def delete(task)
+       @tasks.delete(task)
+       puts "タスクID#{task.id}を削除しました"
+  end
+
 
 end
 
@@ -34,6 +39,9 @@ task1=Task.new(name:"起きる",detail:"８時")
 to_do.create(task1)
 task2=Task.new(name:"ねる",detail:"12時")
 to_do.create(task2)
+to_do.delete(task2)
+task3=Task.new(name:"ランチ",detail:"13時")
+to_do.create(task3)
 to_do.index
 
 
